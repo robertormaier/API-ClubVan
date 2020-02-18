5c002634e4a8a78e2718e73aaa8dcb45a996e8a5
robertomaier02 <robertomaier02@gmail.com.br>
2020-02-17 22:17:37 -0300

    swiger

--- club.van.api/05 - club.van.startup/Startup.cs
+++ club.van.api/05 - club.van.startup/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace club.van.api
 {
@@ -20,6 +21,11 @@ namespace club.van.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Club Van API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +45,13 @@ namespace club.van.api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Club Van API - V1");
             });
 
             loggerFactory.AddFile("Logs/club-van-api-(Date).txt");
